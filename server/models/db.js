b259b97cd1237b9eba8077e3e98065e8f13af97a
Christopher Smith <cms095@email.latech.edu>
2019-11-22 12:47:15 -0600

    Further work on server / database connection error. I am now getting a connection refused error rather than a TransientTransaction error.

--- server/models/db.js
+++ server/models/db.js
@@ -1,8 +1,34 @@
 const mongoose = require('mongoose');
 
+var connectWithRetry = function() {
+
 mongoose.connect(process.env.MONGODB_URI, (err) => {
     if (!err) { console.log('MongoDB connection succeeded.'); }
-    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
+    else { 
+        console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+        setTimeout(connectWithRetry, 5000);
+        console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); 
+        mongoose.connect(process.env.MONGODB_URI);
+    
+    }
 });
+}
+connectWithRetry();
 
 require('./user.model');
+
+
+
+
+// var mongoose = require('mongoose')
+// var mongoUrl = "mongodb://localhost:27017/test"
+
+// var connectWithRetry = function() {
+//   return mongoose.connect(mongoUrl, function(err) {
+//     if (err) {
+//       console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+//       setTimeout(connectWithRetry, 5000);
+//     }
+//   });
+// };
+// connectWithRetry();
