e338ee44c6acd8c9a96130403e8fd9ff380f7da3
Christopher Smith <cms095@email.latech.edu>
2020-02-18 20:58:27 -0600

    added disabled host check to angular_server bash command

--- angular_server/src/environments/environment.ts
+++ angular_server/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://jihyetestenv.duckdns.org:3000/api'
+  apiBaseUrl: 'http://localhost:3000/api'
 };
 /*
  * In development mode, to ignore zone related error stack frames such as
